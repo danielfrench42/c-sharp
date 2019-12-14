63b8f6e5e5904db6fb66cc1ca39c75794877866d
Daniel French <danielfrench42@gmail.com>
2019-12-14 10:58:27 -0800

    Adding dictionaries and strings

--- Program.cs
+++ Program.cs
@@ -25,31 +25,81 @@ namespace c_
             // int num2 = rnd.Next(1,11);
             // Console.WriteLine(num2);
 
-            int[] x = new int[10];
-            int num3 = 0;
-            for(int i = 0; i <= 9; i++)
-            {
+///////////////////////////////
+//
+//      Arrays (Single and 2D)
+//
+///////////////////////////////
+
+            // int[] x = new int[10];
+            // int num3 = 0;
+            // for(int i = 0; i <= 9; i++)
+            // {
                 // Console.WriteLine("i: {0}", i);
-                x[i]= num3;
-                num3++;
-            }
+                // x[i]= num3;
+                // num3++;
+            // }
             // for(int i = 0;i <= 9;i++)
             // {
                 // Console.WriteLine(x[i]);
             // }
             // Console.WriteLine(x.Length); //length of array function (case senstitive)
         
-            int[,] twoD= new int[3,3];  //2D array intialization
+            // int[,] twoD= new int[3,3];  //2D array intialization
 
-            List<int> numbers = new List<int>();
-            numbers.Add(1);
-            numbers.Add(4);
-            numbers.Add(9);
-            Console.WriteLine(numbers.Count);
+////////////////////////////////
+//
+//      Lists and Dictionaries
+//
+////////////////////////////////
+
+
+            // List<int> numbers = new List<int>();
+            // numbers.Add(1);
+            // numbers.Add(4);
+            // numbers.Add(9);
+            // Console.WriteLine(numbers.Count);        //print out numbers in a list
+
             // for(int i = 0; i<= numbers.Count; i++)
             // {
                 // Console.WriteLine(numbers[i]);
             // }
+
+            // Dictionary<string, long> phonebook = new Dictionary<string, long>();    //Dictionary is similar to a list but each entry has a key
+            // phonebook.Add("Bob", 1231234567);                                       //In this case it is a phone number to a name
+
+            // if(phonebook.ContainsKey("Bob"))
+            // {
+            //     Console.WriteLine("Bob's number is {0}", phonebook["Bob"]);
+            // }
+        
+            // phonebook.Remove("Bob");    //remove entry from the list
+
+            // if(phonebook.ContainsKey("Bob"))
+            // {
+            //     Console.WriteLine("Bob's number is {0}", phonebook["Bob"]);
+            // }
+
+////////////////////////////////////
+//
+//      Strings
+//
+////////////////////////////////////
+        
+        string newString = "A new string";
+        
+        String myString = "An equivalent string";   //both create a string
+
+        string emptyString = String.Empty;
+        
+        Console.WriteLine(newString);
+        Console.WriteLine(myString);
+
+        //ToString
+        int num = 2;
+        string makeString = "Being replaced";
+        makeString = num.ToString();
+        System.Console.WriteLine(makeString);
         
         }
 
